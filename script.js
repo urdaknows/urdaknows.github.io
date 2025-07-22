7caf0ada57e313605f5a855cd2b27ea5201d81e7
José Andrés <108591870+urdaknows@users.noreply.github.com>
2025-07-22 12:59:09 -0500

    Updated Html, CSS & JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function isMobile() {
+  return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+function close() {
+  document.getElementById('alert').style.display = 'none';
+}
+
+if (isMobile()) {
+  window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('alert').style.display = 'block';
+  });
+}
