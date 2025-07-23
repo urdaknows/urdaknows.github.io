c915438ae3fcc503e2c45b8c4c91639213157363
José Andrés <108591870+urdaknows@users.noreply.github.com>
2025-07-23 13:46:58 -0500

    .

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ function isMobile() {
   return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-function close() {
-  document.getElementById('alert').style.display = 'none';
-}
-
 if (isMobile()) {
   alert('This site is not optimized for mobile devices. Please use a desktop browser for the best experience.');
 }
